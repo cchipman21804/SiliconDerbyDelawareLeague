01586a07f576b1a83bb624239e53c49adf29a158
Cliff Chipman <61033936+cchipman21804@users.noreply.github.com>
2022-05-16 14:15:35 -0400

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   firstBorn/RaspberryPi4B/raspian32bitOSFull/motorTest.py

--- firstBorn/RaspberryPi4B/raspian32bitOSFull/motorTest.py
+++ firstBorn/RaspberryPi4B/raspian32bitOSFull/motorTest.py
@@ -1,5 +1,48 @@
 from gpiozero import Motor
 # https://gpiozero.readthedocs.io/en/stable/api_output.html#motor
+#from datetime import datetime, timedelta
+from time import sleep
 #
 # This Python script will test the H-bridge and any connected DC motors
 #
+# specify H-Bridge control pins
+fwdL = 0 #10
+revL = 5 #9
+goL = None #11 # optional enable pin
+fwdR = 10 #0
+revR = 9 #5
+goR = None #6 # optional enable pin
+#
+# create Motor class
+motorLeft = Motor(fwdL,revL)
+motorRight = Motor(fwdR,revR)
+#
+# test the motor & H-Bridge
+#
+def stop():
+    # Stop both motors
+    motorLeft.stop()
+    motorRight.stop()
+    sleep(1)
+#
+def straightFwd():
+    # Run both motors forward
+    motorLeft.forward()
+    motorRight.forward()
+#    sleep(1)
+def straightRev():
+    # Run both motors in reverse
+    motorLeft.reverse()
+    motorRight.reverse()
+#    sleep(1)
+#
+# Run forward for 1 second
+stop() # Stops motors for 1 second
+straightFwd()
+sleep(1)
+stop() # Stops motors for 1 second
+#
+# Run in reverse for 1 second
+straightRev()
+sleep(1)
+stop() # Stops motors for 1 second
