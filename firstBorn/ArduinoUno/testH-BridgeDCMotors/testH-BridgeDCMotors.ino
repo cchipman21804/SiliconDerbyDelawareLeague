c7cde5ad3a7da498e38a50e133a291287d31e30d
Cliff Chipman <61033936+cchipman21804@users.noreply.github.com>
2022-05-18 19:59:08 -0400

    Add files via upload

--- firstBorn/ArduinoUno/testH-BridgeDCMotors/testH-BridgeDCMotors.ino
+++ firstBorn/ArduinoUno/testH-BridgeDCMotors/testH-BridgeDCMotors.ino
@@ -18,13 +18,20 @@ void setup() {
   pinMode(revL,OUTPUT);
   pinMode(fwdR,OUTPUT);
   pinMode(revR,OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   STOP();
+  Serial.print("STOPPED");
   delay(1000);
-  
+  Serial.print("STRAIGHT FORWARD");
+  straightFwd();
+  delay(1000);
+  Serial.print("STRAIGHT REVERSE");
+  straightRev();
+  delay(1000);
 }
 
 void STOP() {
@@ -43,11 +50,32 @@ void STOP() {
 }
 
 void straightFwd() {
+// on/off version
+  digitalWrite(fwdL,HIGH);
+  digitalWrite(fwdR,HIGH);
+  digitalWrite(revL,LOW);
+  digitalWrite(revR,LOW);
+
+// PWM speed control version
+//  analogWrite(fwdL,255);
+//  analogWrite(fwdR,255);
+//  analogWrite(revL,0);
+//  analogWrite(revR,0);
   
 }
 
 void straightRev() {
-  
+  // on/off version
+  digitalWrite(fwdL,LOW);
+  digitalWrite(fwdR,LOW);
+  digitalWrite(revL,HIGH);
+  digitalWrite(revR,HIGH);
+
+// PWM speed control version
+//  analogWrite(fwdL,0);
+//  analogWrite(fwdR,0);
+//  analogWrite(revL,255);
+//  analogWrite(revR,255);
 }
 
 void fwdLeft() {
