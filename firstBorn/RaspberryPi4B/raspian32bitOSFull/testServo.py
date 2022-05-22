abb4cb6154f0c60100a65c531461c7e5d17d910b
Cliff Chipman <61033936+cchipman21804@users.noreply.github.com>
2022-05-22 12:00:28 -0400

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   firstBorn/RaspberryPi4B/raspian32bitOSFull/testServo.py

---
+++ firstBorn/RaspberryPi4B/raspian32bitOSFull/testServo.py
@@ -0,0 +1,15 @@
+# Servo Test
+from gpiozero import Servo
+from time import sleep
+
+servo = Servo(4)
+
+while True:
+    servo.min()
+    sleep(1)
+    servo.mid()
+    sleep(1)
+    servo.max()
+    sleep(1)
+    servo.mid()
+    sleep(1)
