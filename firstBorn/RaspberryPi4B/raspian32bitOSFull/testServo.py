797503e93c2f7709853934fd7a33d0944c65d5c3
Cliff Chipman <61033936+cchipman21804@users.noreply.github.com>
2022-05-23 13:44:52 -0400

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   firstBorn/RaspberryPi4B/raspian32bitOSFull/testServo.py

--- firstBorn/RaspberryPi4B/raspian32bitOSFull/testServo.py
+++ firstBorn/RaspberryPi4B/raspian32bitOSFull/testServo.py
@@ -1,9 +1,11 @@
 # Servo Test
 from gpiozero import Servo
 from time import sleep
-
+#
+# hard-coded values for min & max pulse width are suitable for the
+# Parallex Standard Servo (#900-00005) 180 degree movement
 servo = Servo(4,min_pulse_width=0.75/1000,max_pulse_width=2.25/1000)
-
+#
 while True:
     servo.min()
     print("Looking Right")
