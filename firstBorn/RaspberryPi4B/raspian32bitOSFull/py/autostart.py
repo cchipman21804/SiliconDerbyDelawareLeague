d0072aac6e67cd5900eca822d6b341677924ec87
Cliff Chipman <61033936+cchipman21804@users.noreply.github.com>
2022-06-22 18:43:12 -0400

    Update autostart.py

--- firstBorn/RaspberryPi4B/raspian32bitOSFull/py/autostart.py
+++ firstBorn/RaspberryPi4B/raspian32bitOSFull/py/autostart.py
@@ -1,9 +1,9 @@
 import os
 #
 # specify folders
-rootfldr = '/home/pi/SiliconDerbyDelawareLeague/firstBorn/RaspberryPi4B/raspian32bitOSFull'
-diagfldr = '/diagnostics'
-pyfldr = '/py'
+rootfldr = '/home/pi/SiliconDerbyDelawareLeague/firstBorn/RaspberryPi4B/raspian32bitOSFull/'
+diagfldr = 'diagnostics/'
+pyfldr = 'py/'
 #
 # execute multiple Python scripts
-os.system(f"python3 {rootfldr}{diagfldr}/test_OLED_display.py & python3 {rootfldr}{pyfldr}/video-stream.py") # & python3 /var/www/html/wifi-car.py & python3 /var/www/html/system-status.py & python3 /var/www/html/tether.py")
+os.system(f"python3 {rootfldr}{diagfldr}test_OLED_display.py & python3 {rootfldr}{pyfldr}video-stream.py & python3 {rootfldr}{diagfldr}heartBeat.py") # & python3 /var/www/html/wifi-car.py & python3 /var/www/html/system-status.py & python3 /var/www/html/tether.py")
