c2fa60689087644905698b4aa8a5ec45168a1e51
Cliff Chipman <61033936+cchipman21804@users.noreply.github.com>
2022-06-22 18:31:40 -0400

    Update autostart.py

--- firstBorn/RaspberryPi4B/raspian32bitOSFull/py/autostart.py
+++ firstBorn/RaspberryPi4B/raspian32bitOSFull/py/autostart.py
@@ -5,5 +5,5 @@ rootfldr = '/home/pi/SiliconDerbyDelawareLeague/firstBorn/RaspberryPi4B/raspian3
 diagfldr = '/diagnostics'
 pyfldr = '/py'
 #
-# execute Python scripts
-os.system(f"python3 {diagfldr}/test_OLED_display.py & python3 {pyfldr}/video-stream.py") # & python3 /var/www/html/wifi-car.py & python3 /var/www/html/system-status.py & python3 /var/www/html/tether.py")
+# execute multiple Python scripts
+os.system(f"python3 {rootfldr}{diagfldr}/test_OLED_display.py & python3 {rootfldr}{pyfldr}/video-stream.py") # & python3 /var/www/html/wifi-car.py & python3 /var/www/html/system-status.py & python3 /var/www/html/tether.py")
