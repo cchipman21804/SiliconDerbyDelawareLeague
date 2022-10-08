061482a07f113764aa17f9a1dcccdb90c76649f0
Cliff Chipman <61033936+cchipman21804@users.noreply.github.com>
2022-10-08 17:38:06 -0400

    modified:   firstBorn/RaspberryPi4B/raspian32bitOSFull/py/control_v1.0.py

--- firstBorn/RaspberryPi4B/raspian32bitOSFull/py/control_v1.0.py
+++ firstBorn/RaspberryPi4B/raspian32bitOSFull/py/control_v1.0.py
@@ -134,8 +134,8 @@ class MyController(Controller):
         print(f"[{pgmName}] [{dt.datetime.now().strftime('%a %b %d %Y @%H:%M:%S.%f')}]> [{triangleRlsMsg}]")
         stop()
 
-    def on_L2_press():
-        print(f"[{pgmName}] [{dt.datetime.now().strftime('%a %b %d %Y @%H:%M:%S.%f')}]> [L2 pressed]")
+#    def on_L2_press():
+#        print(f"[{pgmName}] [{dt.datetime.now().strftime('%a %b %d %Y @%H:%M:%S.%f')}]> [L2 pressed]")
 #
 # Find all input devices
 inputDevices = os.listdir('/dev/input')
