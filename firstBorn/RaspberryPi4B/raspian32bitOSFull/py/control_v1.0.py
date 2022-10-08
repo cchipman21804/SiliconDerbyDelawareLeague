c9df4d4969a3beeac996a240f916707612cda162
Cliff Chipman <61033936+cchipman21804@users.noreply.github.com>
2022-10-08 17:27:10 -0400

    modified:   firstBorn/RaspberryPi4B/raspian32bitOSFull/py/control_v1.0.py

--- firstBorn/RaspberryPi4B/raspian32bitOSFull/py/control_v1.0.py
+++ firstBorn/RaspberryPi4B/raspian32bitOSFull/py/control_v1.0.py
@@ -135,7 +135,7 @@ class MyController(Controller):
         stop()
 
     def on_L2_press(self):
-        print(f"[{pgmName}] [{dt.datetime.now().strftime('%a %b %d %Y @%H:%M:%S.%f')}]> [{self.value}]")
+        print(f"[{pgmName}] [{dt.datetime.now().strftime('%a %b %d %Y @%H:%M:%S.%f')}]> [{on_L2_press.value}]")
 #
 # Find all input devices
 inputDevices = os.listdir('/dev/input')
