886c9d30e3d1d2b462a573532fca7ea8e06d572b
Cliff Chipman <61033936+cchipman21804@users.noreply.github.com>
2022-10-11 19:11:55 -0400

    modified:   firstBorn/RaspberryPi4B/raspian32bitOSFull/py/findHuman.py

--- firstBorn/RaspberryPi4B/raspian32bitOSFull/py/findHuman.py
+++ firstBorn/RaspberryPi4B/raspian32bitOSFull/py/findHuman.py
@@ -58,47 +58,47 @@ dirR = 27 #27 or 24  # Pin 6 goes HIGH
 motorLeft = PhaseEnableMotor(dirL,goL,pwm=True)
 motorRight = PhaseEnableMotor(dirR,goR,pwm=True)
 #
-print("try it")
-while (True):
-    try:
-        stop() # Stop the motors
-        # Capture frame by frame
-        ret, frame = cap.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#print("try it")
+while (cv2.waitKey(1) & 0xFF == ord('q')): #True):
+#    try:
+    stop() # Stop the motors
+    # Capture frame by frame
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # Display the resulting frame
-        cv2.imshow('Where Is The Human?',frame)
+    # Display the resulting frame
+    cv2.imshow('Where Is The Human?',frame)
 
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.7, minNeighbors=5)
-        for (x,y,w,h) in faces:
-            print(x,y,w,h)
-            roi_gray = gray[y:y+h,x:x+w]
-            roi_color = frame[y:y+h,x:x+w]
-    #
-    # Send this to web server?
-    #        img_item = "myface.png"
-    #        cv2.imwrite(img_item, roi_gray)
-    #
-            color = (255,255,255) # BGR
-            stroke = 4 # line thickness
-            end_x = x+w
-            end_y = y+h
-            cv2.rectangle(frame,(x,y),(end_x,end_y),color,stroke)
-            
-    #        cwSpin()
-    #        sleep(0.25)
-    #        ccwSpin()
-    #        sleep(0.25)
-
-            lmf(0.5) # Move toward the human
-            rmf(0.5)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.7, minNeighbors=5)
+    for (x,y,w,h) in faces:
+        print(x,y,w,h)
+        roi_gray = gray[y:y+h,x:x+w]
+        roi_color = frame[y:y+h,x:x+w]
+#
+# Send this to web server?
+#        img_item = "myface.png"
+#        cv2.imwrite(img_item, roi_gray)
+#
+        color = (255,255,255) # BGR
+        stroke = 4 # line thickness
+        end_x = x+w
+        end_y = y+h
+        cv2.rectangle(frame,(x,y),(end_x,end_y),color,stroke)
         
-        # Press 'Q' to quit
-        #if cv2.waitKey(20) & 0xFF == ord('q'):
-        #    break
+#        cwSpin()
+#        sleep(0.25)
+#        ccwSpin()
+#        sleep(0.25)
 
-    except KeyboardInterrupt:
-        break
+        lmf(0.5) # Move toward the human
+        rmf(0.5)
+    
+    # Press 'Q' to quit
+    #if cv2.waitKey(20) & 0xFF == ord('q'):
+    #    break
+
+#    except KeyboardInterrupt: # Frame window would not display during try:
+#        break
 # Clean up
 cap.release()
 cv2.destroyAllWindows()
